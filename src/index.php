f9c45bd083338799af7a86f8a2cee6d2d797d544
Felix <felix.huel6@gmail.com>
2024-02-13 12:23:26 +0100

    css update

--- src/index.php
+++ src/index.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOME</title>
-    <link rel="stylesheet" href="styling/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOME</title>
+  <link rel="stylesheet" href="styling/style.css">
+  <link rel="stylesheet" href="styling/font.css">
 </head>
+
 <body>
-    
+
+  <header style="background-color: #000; height: 50px;"><a href="" style="color: #f9f002; font-size: 2rem;">ONE PUNCH MAN</a></header>
+
+  <section class="cyberpunk">
+    <h1 class="cyberpunk glitched">Character Dex</h1>
+  </section>
+
+  <section class="cyberpunk black both">
+    <h1 class="cyberpunk">First Name + Last Name</h1>
+    <h2 class="cyberpunk">Alias + Birth Date</h2>
+    <h2 class="cyberpunk">ELO + Rank</h2>
+
+    <img class="cyberpunk" src="https://dummyimage.com/400x500/ff00ff/fff" alt="" />
+    <p class="cyberpunk">Origin Story</p>
+  </section>
+
+  <section class="cyberpunk both">
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+    <img class="cyberpunk" src="https://dummyimage.com/200x300/ff00ff/fff" alt="" />
+</section>
+
 </body>
+
 </html>
