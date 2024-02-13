8ef3589b6d3dfabe62ca54a7bd61d02e43b61eca
Felix <felix.huel6@gmail.com>
2024-02-13 10:10:15 +0100

    styling with plain css instead of sass

--- src/index.php
+++ src/index.php
@@ -7,21 +7,6 @@
     <link rel="stylesheet" href="styling/style.css">
 </head>
 <body>
-
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="index.php">HOME</a>
-  <a href="#">DUEL</a>
-  <a href="#">LEADERBOARD</a>
-</div>
-
-<span onclick="openNav()">|||</span>
-
-<div id="main">
-    <h1 class="test">Test</h1>
-</div> 
-
-<script src="javascript/main.js"></script>
     
 </body>
 </html>
